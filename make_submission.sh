26198d9a879525421c50c543527c073930e7ac18
brian <brianlee0415@gmail.com>
2023-01-05 15:30:03 +0800

    make submission

--- make_submission.sh
+++ make_submission.sh
@@ -5,7 +5,7 @@ mkdir B09902054/codes
 make
 cp cpu_syn.v B09902054/codes
 cp codes/{data_memory,instruction_memory}.v B09902054/codes
-cp codes/{IF,ID,EX}.v B09902054/codes
+cp codes/{IF,ID,EX,cpu}.v B09902054/codes
 cp {cpu.f,cpu.ys,report.pdf} B09902054
 
 zip -r B09902054.zip B09902054
